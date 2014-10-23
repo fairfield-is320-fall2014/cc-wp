98f905c098a56ab5fac1a2df1469a88b5b984cf2
Ryan Wessel <rynjwssl@gmail.com>
2014-10-23 11:42:44 -0400

    Edit functions.php for child theme

--- wp-content/themes/customizr-child/functions.php
+++ wp-content/themes/customizr-child/functions.php
@@ -5,3 +5,4 @@ function enqueue_child_theme_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
 }
+?>
