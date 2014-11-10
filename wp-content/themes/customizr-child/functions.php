5752ac5d8a923b5103a66b387cb1aaa344b8b115
Ryan Wessel <ryan.wessel@student.fairfield.edu>
2014-11-10 11:13:38 -0500

    Added plugins, changed theme to Vantage

--- wp-content/themes/customizr-child/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
-function enqueue_child_theme_styles() {
-    wp_enqueue_style( 'customizr', get_template_directory_uri().'/style.css' );
-    wp_enqueue_style( 'customizr-style', get_stylesheet_uri(), array('customizr')  );
-}
-?>
